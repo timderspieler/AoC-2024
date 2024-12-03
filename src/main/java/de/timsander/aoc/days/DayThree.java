9b4a30ccb53ff4d2b3deb3e66ce80fd8137a79bf
Tim Sander <tim.sander@telekom.de>
2024-12-03 15:26:27 +0100

    Day 3

--- src/main/java/de/timsander/aoc/days/DayThree.java
+++ src/main/java/de/timsander/aoc/days/DayThree.java
@@ -3,7 +3,6 @@ package de.timsander.aoc.days;
 import de.timsander.aoc.DayTemplate;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -13,8 +12,6 @@ import java.util.regex.Pattern;
 import static java.lang.Integer.parseInt;
 
 public class DayThree extends DayTemplate {
-
-    private static final String REGEX = "mul\\(\\d{1,3},\\d{1,3}\\)";
     private final List<String> instructions = new ArrayList<>();
 
     public DayThree() {
@@ -45,29 +42,7 @@ public class DayThree extends DayTemplate {
         var result = 0;
 
         for (var input : instructions) {
-
             result += solve(input, false);
-
-//            var pat = Pattern.compile("(mul\\((\\d+),(\\d+)\\))|(do\\(\\))|(don't\\(\\))");
-//            var matcher = pat.matcher(input);
-//
-//            var enabled = true;
-//
-//            while (matcher.find()) {
-//                if (matcher.group().equals("don't()")) {
-//                    log("Dont activated!");
-//                    enabled = false;
-//                } else if (matcher.group().equals("do()")) {
-//                    log("Do activated!");
-//                    enabled = true;
-//                } else if (enabled) {
-//                    log("Enabled, executing " + matcher.group());
-//                    result += mul(matcher.group());
-//                } else {
-//                    log("Skipping " + matcher.group());
-//                }
-//            }
-
         }
 
         return result;
@@ -81,22 +56,6 @@ public class DayThree extends DayTemplate {
         } catch (Exception e) {
             log(e.getMessage());
         }
-    }
-
-    private int executeMul(String instruction) {
-        var result = 0;
-        var pattern = Pattern.compile(REGEX);
-        var matcher = pattern.matcher(instruction);
-        while (matcher.find()) {
-            result += mul(matcher.group());
-        }
-        return result;
-    }
-
-    private int mul(String mulInstruction) {
-        var numString = mulInstruction.replaceAll("mul\\(", "").replaceAll("\\)", "");
-        var nums = numString.split(",");
-        return parseInt(nums[0]) * parseInt(nums[1]);
     }
 
     private int solve(String input, boolean ignoreConditions) {
